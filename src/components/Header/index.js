4360e162923a4119e09423d702ae5cd49320e9f1
AmirhosseinVeysi <amirhossein_veysi@yahoo.com>
2022-07-11 15:30:14 +0430

    fixed header issue

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,9 @@ import "./Header.css";
 
 const Header = () => (
   <>
-    <nav class="navbar navbar-dark bg-dark py-3 fixed-top">
-      <div class="container-fluid">
-        <span class="navbar-brand mb-0 display-2 ms-3">Phoga</span>
+    <nav className="navbar navbar-dark bg-dark py-3 fixed-top">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 display-2 ms-3">Phoga</span>
       </div>
     </nav>
   </>
