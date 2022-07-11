029e7331d8cf6d14a382bfec5aff82655d9476d4
AmirhosseinVeysi <amirhossein_veysi@yahoo.com>
2022-07-11 12:05:45 +0430

    Updated navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,9 +3,9 @@ import "./Header.css";
 
 const Header = () => (
   <>
-    <nav class="navbar navbar-light bg-light">
+    <nav class="navbar navbar-dark bg-dark py-3 fixed-top">
       <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1">Phoga</span>
+        <span class="navbar-brand mb-0 display-2 ms-3">Phoga</span>
       </div>
     </nav>
   </>
